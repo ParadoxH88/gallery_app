fadd8ce74b2db47bd624774043e0f642e449a41c
ParadoxH88 <paradosz@gmail.com>
2021-03-25 23:35:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+    let imagesData = [
+        {
+            photo: `./img/1.jpg`,
+            title: 'Első, de nem utolsó.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+        {
+            photo: './img/2.jpg',
+            title: 'Szépen csillog az ezüst is.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+        {
+            photo: './img/3.jpg',
+            title: 'Bronz',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+
+        {
+            photo: './img/4.jpg',
+            title: 'Elso, de nem utolso.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+        {
+            photo: './img/5.jpg',
+            title: 'Nem első és nem utolsó.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+        {
+            photo: './img/6.jpg',
+            title: 'A hatodik se rossz.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+
+        {
+            photo: './img/7.jpg',
+            title: 'Utolsókból lesznek az elsők.',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
+                },
+ 
+    ]
+
+let currentPhoto = 0;
+
+let loadImage = (ind) => {
+    $('#picture').attr('src', imagesData[ind].photo);
+    $('#phototitle').text(imagesData[ind].title);
+    $('#descript').text(imagesData[ind].description);
+}
+
+$('#forward').click(() => {
+    currentPhoto++;
+    loadImage(currentPhoto);
+});
+
+$('#back').click (()  => {
+    currentPhoto--;
+    loadImage(currentPhoto);
+});
+
+$('.smallpic').click(() => {
+    $('#picture').attr('src', imagesData[ind].photo);
+});
